da0e7afb99b2f50a6c6815b7caaddaed2612f80d
bradschwartz <baschwartz95@gmail.com>
2023-09-18 16:48:16 -0700

    Unit tests + runner updates for get

---
+++ src/__tests__/get.test.js
@@ -0,0 +1,25 @@
+import get from '../get';
+
+describe('get', () => {
+
+  it('returns null if object is not an object', () => {
+    expect(get(null, 'a')).toBeNull();
+    expect(get(123, 'a')).toBeNull();
+  });
+
+  it('returns the value at the given key', () => {
+    const obj = { a: { b: 2 } };
+    expect(get(obj, 'a')).toEqual({ b: 2 });
+  });
+
+  it('returns nested value if multiple keys provided', () => {
+    const obj = { a: { b: { c: 3 } } };
+    expect(get(obj, 'a', 'b', 'c')).toBe(3);
+  });
+
+  it('returns undefined if key does not exist', () => {
+    const obj = { a: 1 };
+    expect(get(obj, 'b')).toBeUndefined();
+  });
+
+});
