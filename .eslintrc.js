9f8f66a8be7657d3c2417ac333f5631aa0092eaa
bradschwartz <baschwartz95@gmail.com>
2023-09-18 16:56:22 -0700

    Add babel/eslint and configure. No lint issues!

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    requireConfigFile: false,
+
+    babelOptions: {
+      presets: ["@babel/preset-react"],
+    },
+  },
+};
